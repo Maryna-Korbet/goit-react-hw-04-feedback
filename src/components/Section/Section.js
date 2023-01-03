3c68a0d29c30efabcc1bd9eb51ba184320d8df1e
Maryna-Korbet <m22v86k@gmail.com>
2023-01-04 01:36:16 +0200

    added Section.propTypes

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,4 +1,5 @@
 import css from 'components/Section/Section.module.css';
+import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
   return (
@@ -7,6 +8,11 @@ const Section = ({ title, children }) => {
       {children}
     </section>
   );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
 };
 
 export default Section;
